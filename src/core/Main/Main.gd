ebfd21d12c246c5e2091415fca4b9a4ec1a43356
SaviHex <leosavis@gmail.com>
2020-07-11 10:31:01 -0300

    Inimigo agora atira no player

--- src/core/Main/Main.gd
+++ src/core/Main/Main.gd
@@ -9,10 +9,15 @@ func _ready() -> void:
 	self.characters = $Characters.get_children()
 	init_enemies()
 
+func init_player():
+	pass
 
 func init_enemies():
 	var i = 0
 	for c in self.characters:
 		if c.is_in_group("enemy"):
-			c.init(self.paths[i])
+			c.init(self.paths[i], get_next_char(i))
 		i += 1
+
+func get_next_char(i: int) -> Node2D:
+	return self.characters[(i + 1) % self.characters.size()]
--- src/core/enemy/Enemy.gd
+++ src/core/enemy/Enemy.gd
@@ -1,67 +1,41 @@
 extends KinematicBody2D
 
-export var speed: float = 50
-var friction = 0.2
-var acceleration = 0.5
-var velocity = Vector2.ZERO
+export var speed: float = 1.0
+export var min_shoot_time: float = 1.0
+export var max_shoot_time: float = 5.0
 
-onready var pivot = get_node("Pivot")
-onready var bullet_point = get_node("Pivot/BulletPoint")
+onready var pivot = $Pivot
+onready var bullet_point = $Pivot/BulletPoint
+onready var shoot_timer: Timer = $ShootTimer
 
 var bullet = preload("res://core/bullet/Bullet.tscn")
 
-var main_node: Node2D
+var initialized: bool = false
 var current_path
-
-var path_positions = Array()
-var current_pos : int = 0
+var current_target: Node2D
 
 func _ready():
-	self.main_node = get_parent().get_parent()
-#
-#	var path_node_parent
-#	for node in get_parent().get_parent().get_node("Characters/AiPath").get_children():
-#		if node.get_name() == get_name():
-#			path_node_parent = node
-#			break
-#
-#	for node in path_node_parent.get_children():
-#		path_positions.append(node)
+	randomize()
+	var random_time = rand_range(self.min_shoot_time, self.max_shoot_time)
+	self.shoot_timer.wait_time = random_time
+	self.shoot_timer.start()
 
-
-func init(path: Path2D):
+func init(path: Path2D, target: Node2D):
 	self.current_path = path
 	self.current_path.start(self.speed)
-	print("initialized")
+	self.current_target = target
+	self.initialized = true
 
 func _process(delta: float) -> void:
-	if self.current_path != null:
+	if self.initialized:
 		self.position = self.current_path.get_current_point()
-
-#
-#func get_target():
-#	var target_pos = path_positions[current_pos].position
-#	if abs(target_pos.y - position.y) < 10 and abs(target_pos.x - position.x) < 10:
-#			current_pos += 1
-#			if current_pos > path_positions.size() - 1:
-#				current_pos = 0
-#	return target_pos
-#
-#func _physics_process(delta):
-#	var target_pos = get_target()
-#	var input_velocity = Vector2.ZERO
-#
-#	if target_pos:
-#		pivot.look_at(target_pos)
-#		input_velocity = (target_pos - position).normalized() * speed
-#
-#	if input_velocity.length() > 0:
-#		velocity = velocity.linear_interpolate(input_velocity, acceleration)
-#	else:
-#		velocity = velocity.linear_interpolate(Vector2.ZERO, friction)
-#	velocity = move_and_slide(velocity)
+		self.pivot.look_at(self.current_target.position)
 
 func shoot():
 	var b = bullet.instance()
 	b.start(bullet_point.global_position, pivot.rotation)
 	get_parent().add_child(b)
+
+
+func _on_ShootTimer_timeout() -> void:
+	shoot()
