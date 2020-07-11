b7338609ec4af74bf134b83ca8a9391e590e5b20
Guilherme Martins Marques <guimm121197@gmail.com>
2020-07-11 00:08:59 -0300

    Added bullet, added shooting

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -1,14 +1,21 @@
 extends KinematicBody2D
 
-var speed = 250
+var speed = 150
 var friction = 0.2
 var acceleration = 0.5
 var velocity = Vector2.ZERO
 
+onready var pivot = get_node("Pivot")
+onready var bullet_point = get_node("Pivot/BulletPoint")
+
+var bullet = preload("res://bullet/Bullet.tscn")
+
 func _physics_process(delta):
+	pivot.look_at(get_global_mouse_position())
+	
 	var input_velocity = Vector2.ZERO
 	
-	# Input
+	# Movement Input
 	if Input.is_action_pressed("right"):
 		input_velocity.x += 1
 	if Input.is_action_pressed("left"):
@@ -18,6 +25,10 @@ func _physics_process(delta):
 	if Input.is_action_pressed("up"):
 		input_velocity.y -= 1
 	input_velocity = input_velocity.normalized() * speed
+	
+	# Shooting input
+	if Input.is_action_just_pressed('mouse_click'):
+		shoot()
 
 	# If there's input, accelerate to the input velocity
 	if input_velocity.length() > 0:
@@ -26,3 +37,9 @@ func _physics_process(delta):
 		# If there's no input, slow down to (0, 0)
 		velocity = velocity.linear_interpolate(Vector2.ZERO, friction)
 	velocity = move_and_slide(velocity)
+
+func shoot():
+	# "Muzzle" is a Position2D placed at the barrel of the gun.
+	var b = bullet.instance()
+	b.start(bullet_point.global_position, pivot.rotation)
+	get_parent().add_child(b)
