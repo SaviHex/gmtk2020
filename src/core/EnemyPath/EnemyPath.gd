2840161bbe34eeed7465b82d7963c034629a9f7f
SaviHex <leosavis@gmail.com>
2020-07-11 18:37:42 -0300

    Bug do início

--- src/core/EnemyPath/EnemyPath.gd
+++ src/core/EnemyPath/EnemyPath.gd
@@ -7,6 +7,9 @@ export var color: Color = Color.white setget set_color
 var speed: float = 1.0
 var is_stopped: bool = true
 
+func _draw() -> void:
+	draw_polyline(curve.get_baked_points(), color, 2.0)
+
 func start(new_speed: float = 1.0):
 	self.speed = new_speed
 	self.is_stopped = false
@@ -27,5 +30,3 @@ func get_current_point() -> Vector2:
 
 func set_color(value: Color):
 	color = value
-	color.a = 0.5
-	self.self_modulate = color
