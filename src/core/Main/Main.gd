f1002afed8ce4c594bd4e435c10fb50e8c322a48
Guilherme Martins Marques <guimm121197@gmail.com>
2020-07-11 11:16:04 -0300

    Added basic level transition

--- src/core/Main/Main.gd
+++ src/core/Main/Main.gd
@@ -1,13 +1,24 @@
 extends Node2D
 
+export(String) var next_level
+export(int) var wait_time
+
+onready var timer = get_node("Timer")
+onready var time_left_label = get_node("Timer/TimeLeft")
 
 var paths: Array
 var characters: Array
 
 func _ready() -> void:
+	timer.wait_time = wait_time
+	timer.start()
+	
 	self.paths = $EnemyPaths.get_children()
 	self.characters = $Characters.get_children()
 	init_enemies()
+
+func _physics_process(delta):
+	time_left_label.set_text(str(floor(timer.time_left)))
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.scancode == KEY_SPACE and event.is_pressed():
@@ -23,7 +34,6 @@ func init_enemies():
 func get_next_char(i: int) -> Node2D:
 	return self.characters[(i + 1) % self.characters.size()]
 
-
 func switcheroo():
 	var i = 0
 	for c in self.characters:
@@ -37,3 +47,5 @@ func switcheroo():
 
 	init_enemies()
 
+func _on_Timer_timeout():
+	get_tree().change_scene(next_level)
