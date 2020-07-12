c6118a66600ba9790150dd6c45c8040f9dd1fba9
Guilherme Martins Marques <guimm121197@gmail.com>
2020-07-12 13:20:02 -0300

    mais pequenos ajustes

--- src/menus/End/End.gd
+++ src/menus/End/End.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _physics_process(delta):
 	MusicManager.time_str
-	var time_text = "[center]\"What are you kids doing?? I leave for just [color=#d95763]" + MusicManager.time_str + "[/color] seconds and you are already messing with the interdimentional guns!\"[/center]"
+	var time_text = "[center]\"What are you kids doing?? I leave for just [color=#d95763]" + MusicManager.time_str + "[/color] seconds and you are already all fighting? Thank you [color=#d95763]player[/color], for keeping the situation in control."
 	$Title.bbcode_text = time_text
 
 func _on_ExitButton_pressed():
